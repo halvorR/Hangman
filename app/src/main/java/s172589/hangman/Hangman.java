f840ff9d8d4ec94a5877e6149d2ea29f78d54fbf
halvorR <halvorr91@gmail.com>
2015-09-16 22:08:23 +0200

    Småfiks! Lagt til valg når spiller taper, samt alt av funksjonalitet

--- app/src/main/java/s172589/hangman/Hangman.java
+++ app/src/main/java/s172589/hangman/Hangman.java
@@ -1,5 +1,8 @@
 package s172589.hangman;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,7 +17,7 @@ import android.widget.TextView;
 import java.util.Random;
 
 // Selve Hangman-aktiviteten
-public class Hangman extends AppCompatActivity{
+public class Hangman extends AppCompatActivity {
     // Visning av ord og alfabet
     private String[] ord;
     private Random tilfeldig;
@@ -30,6 +33,11 @@ public class Hangman extends AppCompatActivity{
     private int gjeldendeKroppsdel;
     private int antallBokstaver;
     private int antallRiktigGjettet;
+
+    // Historikk
+    private int gangerSpilt;
+    private int gangerTapt;
+    private int gangerVunnet;
 
 
     @Override
@@ -104,6 +112,60 @@ public class Hangman extends AppCompatActivity{
                 bokstavVisning[i].setTextColor(Color.BLACK);
             }
         }
+        if(finnes) {
+            // Gjettet riktig
+            // Trengs vel egentlig ikke
+        } else if(gjeldendeKroppsdel < antallKroppsdeler) {
+            kroppsdeler[gjeldendeKroppsdel].setVisibility(v.VISIBLE);
+            gjeldendeKroppsdel++;
+            System.out.println(gjeldendeKroppsdel + " | " + antallKroppsdeler);
+        }
+        if(gjeldendeKroppsdel == antallKroppsdeler){
+            slåAvKnapper();
+
+            // Dialogboks for å gi brukeren svaret, og valget på hva de vil gjøre videre
+            System.out.println("Brukeren har tapt");
+
+            AlertDialog.Builder valgVedTap = new AlertDialog.Builder(this);
+            valgVedTap.setTitle(R.string.tapTittel);
+            valgVedTap.setMessage(R.string.tapMelding1 + gjeldendeOrd + R.string.tapMelding2);
+            valgVedTap.setPositiveButton(R.string.tapNyttSpill,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Hangman.this.spill();
+                        }
+                    });
+
+            valgVedTap.setNegativeButton(R.string.tapTilHovedmeny,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                           Hangman.this.tilStartside();
+
+                        }
+                    });
+
+            valgVedTap.show();
+
+        }
+
+        if(antallRiktigGjettet == antallBokstaver){
+            slåAvKnapper();
+
+
+            System.out.println("Spillet er ferdig, bruker har vunnet");
+        }
+    }
+
+    public void slåAvKnapper(){
+        int antall = bokstaver.getChildCount();
+        for(int i = 0; i < antall; i++){
+            bokstaver.getChildAt(i).setEnabled(false);
+        }
+    }
+
+    public void tilStartside(){
+        Intent i = new Intent(this, StartMenu.class);
+        startActivity(i);
     }
 
     @Override
